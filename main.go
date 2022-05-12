60f8d1d784fd48eccd23d3be5c31d536c0c5f022
Ali Sharafi <a.sharafi1993@gmail.com>
2022-05-12 08:54:49 +0430

    Completed base of server

--- main.go
+++ main.go
@@ -1,34 +1,35 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 
-	"github.com/gin-gonic/gin"
+	"github.com/ali-sharafi/wallet/routes"
+	"github.com/ali-sharafi/wallet/settings"
 )
 
-// album represents data about a record album.
-type album struct {
-	ID     string  `json:"id"`
-	Title  string  `json:"title"`
-	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
+func init() {
+	settings.Setup()
 }
 
 func main() {
-	router := gin.Default()
-	router.GET("/albums", getAlbums)
+	routeHandler := routes.InitRouter()
 
-	router.Run("localhost:8080")
-}
+	endPoint := fmt.Sprintf(":%d", settings.ServerSetting.HttpPort)
+	readTimeout := settings.ServerSetting.ReadTimeout
+	writeTimeout := settings.ServerSetting.WriteTimeout
+	maxHeaderBytes := 1 << 20
 
-// albums slice to seed record album data.
-var albums = []album{
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
-}
+	server := &http.Server{
+		Handler:        routeHandler,
+		Addr:           endPoint,
+		ReadTimeout:    readTimeout,
+		WriteTimeout:   writeTimeout,
+		MaxHeaderBytes: maxHeaderBytes,
+	}
 
-// getAlbums responds with the list of all albums as JSON.
-func getAlbums(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, albums)
+	log.Printf("[info] start http server listening %s", endPoint)
+
+	server.ListenAndServe()
 }
