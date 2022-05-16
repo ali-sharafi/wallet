3325b10b7df82f5297aaa802a4f4c638b7507220
Ali Sharafi <a.sharafi1993@gmail.com>
2022-05-16 23:18:23 +0430

    Added jwt middleware

--- routes/api.go
+++ routes/api.go
@@ -2,10 +2,13 @@ package routes
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/ali-sharafi/wallet/controllers"
 	"github.com/ali-sharafi/wallet/pkg/logging"
+	"github.com/ali-sharafi/wallet/pkg/utils"
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt"
 )
 
 func InitRouter() *gin.Engine {
@@ -15,6 +18,7 @@ func InitRouter() *gin.Engine {
 	r.POST("/auth", controllers.GetAuth)
 
 	apiV1 := r.Group("/api/v1/wallets")
+	apiV1.Use(JWT())
 
 	apiV1.GET("/", controllers.GetWallets)
 
@@ -30,6 +34,40 @@ func InitRouter() *gin.Engine {
 func RequestLogger() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		logging.Log(fmt.Sprintf("New request from %v", c.Request.URL))
+		c.Next()
+	}
+}
+
+func JWT() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var code int
+		var err error
+		var claims *jwt.StandardClaims
+
+		code = http.StatusOK
+		token := c.GetHeader("Authorization")
+
+		if token == "" {
+			code = http.StatusUnauthorized
+		} else {
+			claims, err = utils.ParseToken(token)
+			if err != nil {
+				code = http.StatusUnauthorized
+			}
+		}
+
+		if code != http.StatusOK {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"code": code,
+				"msg":  "unauthorized",
+				"data": "",
+			})
+
+			c.Abort()
+			return
+		}
+
+		c.Set("user_id", claims.Issuer)
 		c.Next()
 	}
 }
